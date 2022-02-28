760a18e8ec20808dc9f147d364991c2b5f1ff29d
Facualemandi <facuinstituto10@gmail.com>
2022-02-27 23:16:32 -0300

    Actualizaciones

--- code.js
+++ code.js
@@ -4,19 +4,60 @@
     hamburger.classList.toggle("is-active");
   });
 
+
+  const expresiones = {
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+
+  }
+
 const d = document;
 d.addEventListener('DOMContentLoaded', (e)=>{
-   openMenu('.hamburger', '.hamburger-inner', '.hamburger-box')
+   openMenu('.hamburger', '.hamburger-inner', '.hamburger-box', '#li', '#start','#email')
 })
 
+const nav = document.getElementById('nav');
+const segurity = document.getElementById('segurity');
+const send = document.getElementById('send')
+const error = document.getElementById('error')
+const validar = document.querySelector('.email')
+const verificar = document.getElementById('verificar')
 
-function openMenu(open,inner,box){
+
+function openMenu(open,inner,box,li, start,email){
  const d = document;
     d.addEventListener('click', (e) =>{
         if(e.target.matches(open) || e.target.matches(inner) || e.target.matches(box)){
-            const nav = document.getElementById('nav');
-             nav.classList.toggle('is-active');
             
-        } 
+             nav.classList.toggle('is-active');
+             nav.classList.remove('esconderNav')
+
+            
+        } if(e.target.matches(li)){
+             nav.classList.toggle('esconderNav');
+             nav.classList.remove('is-active');
+       
+        }if(e.target.matches(start)){
+             segurity.style.display = "block"
+           setTimeout(()=>{
+            segurity.style.display = "none"
+           },1500 )
+
+        }if(e.target.matches(email) && expresiones.correo.test(validar.value) ){
+            verificar.style.display = "block"
+            send.style.display = "none"
+            setInterval(()=>{
+              send.style.display = "block"
+              verificar.style.display = "none"
+             },1500 )
+
+
+              e.preventDefault()
+        }if(e.target.matches(email) && validar.value === "" ){
+           verificar.style.display = "block"
+          setTimeout(()=>{
+            error.style.display = "block"
+            verificar.style.display = "none"
+           },1500 )
+        }
     })
 }
